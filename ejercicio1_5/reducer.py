30c916b97b63f58b3b263d5181c392eeace8785e
PedroPatinoGarcia <pedropatino.dev@gmail.com>
2023-12-01 21:31:08 +0100

    Ejercicio 5

--- ejercicio1_5/reducer.py
+++ ejercicio1_5/reducer.py
@@ -13,23 +13,9 @@ oldKey = None
 # then the key will change and we'll be dealing with the next store
 
 for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
-        # Something has gone wrong. Skip this line.
-        continue
+    thisSale = line.strip()
 
-    thisKey, thisSale = data_mapped
-
-    # Escribe un par key:value ante un cambio na key
-    # Reinicia o total
-    if oldKey and oldKey != thisKey:
-        print(oldKey+"\t"+str(salesTotal))
-        oldKey = thisKey;
-        salesTotal = 0
-
-    oldKey = thisKey
     salesTotal += float(thisSale)
 
 # Escribe o ultimo par, unha vez rematado o bucle
-if oldKey != None:
-    print(oldKey+"\t"+str(salesTotal))
+print(str(salesTotal))
